eb210c2967da08ae54d8f805c80e541449c49cf9
blxk11 <vyshnavkv106@gmail.com>
2026-07-06 00:09:47 +0530

    buy stock

---
+++ buystock.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_prices = prices[0]
+        max_prices = 0
+
+        for i in prices:
+            if i < min_prices:
+                min_prices = i
+            else:
+                profit = i - min_prices
+                if profit > max_prices:
+                    max_prices = profit
+
+        return max_prices  # ✅ outside the loop
